2d0cd381d4485f680c0263dc56634d312c6a7100
vfonsecao <65827402+vfonsecao@users.noreply.github.com>
2025-08-12 18:01:28 -0300

    Delete BFD_BACK_END_PYTHON/aula02 directory

--- BFD_BACK_END_PYTHON/aula02/Ano_Nascimento.py
+++
@@ -1,3 +0,0 @@
-ano_atual = 2025
-ano_nascimento = int(input("Digite o ano do seu nascimento: "))
-print(f"Você tem {ano_atual - ano_nascimento} anos")
--- BFD_BACK_END_PYTHON/aula02/Input.py
+++
@@ -1,7 +0,0 @@
-nome_usuario = input("Qual é seu nome? ")
-print("Prazer em te conhecer" , nome_usuario)
-# Saída esperada: Prazer em te conhecer, Victor
-
-email_usuario = input("Digite seu e-mail: ")
-print(email_usuario)
-# Saída esperada: vfonseca132@gmail.com

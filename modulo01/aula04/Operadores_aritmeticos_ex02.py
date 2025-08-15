b50dc64245e74bc314908c33f3ee634abe07a34c
vfonsecao <65827402+vfonsecao@users.noreply.github.com>
2025-08-15 20:35:15 -0300

    Add files via upload

---
+++ modulo01/aula04/Operadores_aritmeticos_ex02.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite o número 1:"))
+num2 = float(input("Digite o número 2:"))
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"{num1} * {num2} = {multiplicacao}")
+print(f"{num1} / {num2} = {divisao}")

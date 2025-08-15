420d345afa662001c198adc07da1779c98641c8b
vfonsecao <65827402+vfonsecao@users.noreply.github.com>
2025-08-15 20:50:59 -0300

    Add files via upload

---
+++ modulo01/aula04/Operadores_aritmeticos_ex03.py
@@ -0,0 +1,7 @@
+dividendo = int(input("Digite o número 1:"))
+divisor = int(input("Digite o número 2:"))
+
+resto = dividendo % divisor
+resultado = dividendo // divisor
+
+print(f"{dividendo} / {divisor} = {resultado} | resto = {resto}")
---
+++ modulo01/aula04/Operadores_aritmeticos_ex04.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite o número 1:"))
+num2 = int(input("Digite o número 2:"))
+
+potencia = num1 ** num2
+divisao_inteira = num1 // num2
+
+print(f"{num1} ** {num2} = {potencia}")
+print(f"{num1} // {num2} = {divisao_inteira}")

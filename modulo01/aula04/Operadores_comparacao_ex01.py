d8ae1dd8622fee4a7dd30d5ef51b16648fabfdc9
vfonsecao <65827402+vfonsecao@users.noreply.github.com>
2025-08-16 15:53:27 -0300

    Add files via upload

---
+++ modulo01/aula04/Operadores_comparacao_ex01.py
@@ -0,0 +1,5 @@
+nome_a = "Python"
+nome_b = "python"
+
+operação_de_comparação = bool(nome_a == nome_b)
+print(f"{nome_a} é igual ao {nome_b}? {operação_de_comparação}")
---
+++ modulo01/aula04/Operadores_comparacao_ex02.py
@@ -0,0 +1,5 @@
+valor1 = 10.50
+valor2 = 12.00
+
+operação_de_comparação = bool(valor1 != valor2)
+print(f"{valor1} é diferente do {valor2}? {operação_de_comparação}")

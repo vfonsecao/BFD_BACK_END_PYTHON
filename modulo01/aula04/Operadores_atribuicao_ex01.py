b482830f786f2883b69f3b5fc5447ccc5fa330c5
vfonsecao <65827402+vfonsecao@users.noreply.github.com>
2025-08-15 21:02:47 -0300

    Add files via upload

---
+++ modulo01/aula04/Operadores_atribuicao_ex01.py
@@ -0,0 +1,8 @@
+estoque = 50
+print(f"Estoque inicial: {estoque}")
+
+valor = int(input("Informe a quantidade acrescentada ao estoque:"))
+estoque += valor
+
+print(f"Estoque final: {estoque}")
+
---
+++ modulo01/aula04/Operadores_atribuicao_ex02.py
@@ -0,0 +1,6 @@
+numero = 2
+print(f"Valor inicial: {numero}")
+
+expoente = float(input("Digite aqui o expoente:"))
+numero **= expoente
+print(f"Valor final: {numero}")
